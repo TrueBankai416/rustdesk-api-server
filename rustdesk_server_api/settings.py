8d80393ba5a77bcb39f77dec43c8116929c220f7
TrueBankai416 <97103466+TrueBankai416@users.noreply.github.com>
2025-03-21 16:05:00 -0400

    Update settings.py

--- rustdesk_server_api/settings.py
+++ rustdesk_server_api/settings.py
@@ -48,8 +48,8 @@ MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", '-')
 MYSQL_PORT = os.environ.get("MYSQL_PORT", '3306')
 # ==========数据库配置 结束=====================
 
-LANGUAGE_CODE = os.environ.get("LANGUAGE_CODE", 'zh-hans')
-# #LANGUAGE_CODE = os.environ.get("LANGUAGE_CODE", 'en')
+#LANGUAGE_CODE = os.environ.get("LANGUAGE_CODE", 'zh-hans')
+LANGUAGE_CODE = os.environ.get("LANGUAGE_CODE", 'en')
 
 # Application definition
 
